3dcec1eab081a4a6191e982e88ff9811db4a4ab3
Ingy döt Net <ingy@ingy.net>
2016-11-22 09:13:36 -0800

    Fix error message. Update tests.

--- test/test.bash
+++ test/test.bash
@@ -8,9 +8,12 @@ run() {
   echo \
 ================================================================================
   echo "= Run: $*"
+  if [[ -n $error ]]; then
+    echo Expect an error.
+  fi
   echo \
 ================================================================================
-  "$@"
+  "$@" || true
 
   if [[ -n $after ]]; then
     echo "$ $after"
@@ -22,10 +25,12 @@ run() {
 
 run dockerbrew
 # run dockerbrew upgrade
+error=1 \
+  run dockerbrew bogus
 run dockerbrew search cowsay
 after='type cowsay' \
   run dockerbrew use cowsay
-run dockerbrew install cowsay --as=cowtalk
-run dockerbrew uninstall cowtalk
+run dockerbrew install cowsay
+run dockerbrew uninstall cowsay
 run dockerbrew cmd cowsay Hi
 run dockerbrew run cowsay Hi
